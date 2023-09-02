d30e3e219d983bf59f344c322c753be95bfcc021
aososa32 <140859888+aososa32@users.noreply.github.com>
2023-09-02 16:01:31 -0600

    Delete producto.js

--- producto.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const productoSchema = new mongodb.Schema({
-  city: String,
-  vin: String,
-  county: String,
-});
-
-module.exports = mongodb.model('Producto', productoSchema);
